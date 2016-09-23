bbdf454d9d1aa31b0124b5c90b3f4ee801cd04db
michael-mj-john <mbjohn@ucsc.edu>
2016-09-22 22:35:37 -0700

    Hello, Walt.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, World!" << endl;
+	cout << "Coding is fun\n";
+	cout << "see out\n";
+
+	system("pause");
+
+	return 0;
+
+}
